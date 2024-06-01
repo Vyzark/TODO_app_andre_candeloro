cd262907030b48451bb24c56d8253194f201a02d
vyzark <candeloro95@gmail.com>
2024-06-01 22:12:37 +0200

    Created basic HTML structure and test todo list inside data.js

---
+++ js/data.js
@@ -0,0 +1,46 @@
+const todoList = [
+    {
+        id: 1,
+        task: "Buy groceries",
+        priority: "daily",
+    },
+    {
+        id: 2,
+        task: "Pay rent",
+        priority: "monthly",
+    },
+    {
+        id: 3,
+        task: "Schedule doctor's appointment",
+        priority: "urgent",
+    },
+    {
+        id: 4,
+        task: "Clean the house",
+        priority: "daily",
+    },
+    {
+        id: 5,
+        task: "Finish project report",
+        priority: "urgent",
+    },
+    {
+        id: 6,
+        task: "Water the plants",
+        priority: "daily",
+    },
+    {
+        id: 7,
+        task: "Renew car insurance",
+        priority: "monthly",
+    },
+    {
+        id: 8,
+        task: "Prepare presentation for meeting",
+        priority: "urgent",
+    },
+];
+
+// Create:
+// 1. filterByName --> for the <select> filter
+// 2. filterByPriority --> for <input> or <search> (choose one) text input
