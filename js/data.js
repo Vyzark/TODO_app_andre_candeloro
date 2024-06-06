903d1a9f11400ef6f6599aa9b579cf82033b02d4
vyzark <candeloro95@gmail.com>
2024-06-06 09:08:53 +0200

    Small fix to HTML. Created basic functions, constants and events.

--- js/data.js
+++ js/data.js
@@ -14,26 +14,6 @@ const todoList = [
         task: "Schedule doctor's appointment",
         priority: "urgent",
     },
-    {
-        id: 4,
-        task: "Clean the house",
-        priority: "daily",
-    },
-    {
-        id: 6,
-        task: "Water the plants",
-        priority: "daily",
-    },
-    {
-        id: 7,
-        task: "Renew car insurance",
-        priority: "monthly",
-    },
-    {
-        id: 8,
-        task: "Prepare presentation for meeting",
-        priority: "urgent",
-    },
 ];
 
 const doneList = [
@@ -43,22 +23,6 @@ const doneList = [
         priority: "urgent",
     },
 ];
-
-//* HTML DELETE Button Model
-// Add span elements with dom.innerHTML
-{
-    /* <button class="noSelect">
-<span class="text">Delete</span>
-<span class="icon">
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-        viewBox="0 0 24  24">
-        <path
-            d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z">
-        </path>
-    </svg>
-</span>
-</button> */
-}
 
 // Separate TODO's into lists:
 // Create a function that will move check marked items from one list to the other (use splice?)
@@ -87,6 +51,7 @@ function filterByName(list, text) {
     );
 }
 
+// Model - Check functionality
 function filterByPriority(list, domElem) {
-    return list.filter(todo => todo.priority === domElem.value);
+    return list.filter((todo) => todo.priority === domElem.value);
 }
--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,84 @@
-// Use event of type "input" (oninput) for the filterByName input field
+//TODO: Replace the "+" button with an "ADD" styled to "merge" it together with the input field
+
+//* --------------------------GLOBAL CONST DECLARATION----------------------------------------->>
+const formTop = document.querySelector("#formTop");
+const filterSelect = document.querySelector("#filterBottom select");
+const filterInput = document.querySelector("#filterBottom input");
+const todoDiv = document.querySelector(".todoContainer>.todo");
+const doneDiv = document.querySelector(".todoContainer>.done");
+
+let idCounter = 1; // Necessary? Use in addTodo
+
+//* --------------------------LISTENERS----------------------------------------->>
+formTop.addEventListener("submit", addTodo);
+filterSelect.addEventListener("change", priorityFilter);
+filterInput.addEventListener("input", nameFilter);
+
+//* --------------------------EVENT FUNCTIONS----------------------------------------->>
+function addTodo(event) {
+    event.preventDefault();
+    const localForm = event.target;
+
+    printAllTodo(todoList, todoDiv);
+
+    localForm.reset();
+}
+function priorityFilter(event) {}
+function nameFilter(event) {}
+
+//* --------------------------PRINT FUNCTIONS----------------------------------------->>
+
+//* =========== PRINT TO-DO'S ===========
+function printOneTodo(todo, domObj) {
+    const div = document.createElement("div");
+    const input = document.createElement("input");
+    const label = document.createElement("label");
+
+    console.log(todo);
+
+    input.type = "checkbox";
+    input.id = `item${todo.id}`;
+
+    label.for = todo.id; // --> Check if value is correct
+    label.classList.add(todo.priority);
+    label.textContent = todo.task;
+    label.innerHTML = `
+    <button class="noSelect">
+        <span class="text">Delete</span>
+        <span class="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                viewBox="0 0 24  24">
+                <path
+                    d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z">
+                </path>
+            </svg>
+        </span>
+    </button>
+    `;
+
+    div.append(input, label);
+    domObj.appendChild(div);
+}
+function printAllTodo(list, domObj) {
+    list.forEach((item) => printOneTodo(item, domObj));
+}
+
+//* =========== PRINT PRIORITY <OPTION> TAGS ===========
+function printOneOption(item, domObj) {
+    const option = document.createElement("option");
+
+    option.textContent = capitalize(item.priority);
+    option.value = item.priority;
+
+    domObj.appendChild(option);
+}
+function printAllOptions(list, domObj) {
+    list.forEach((option) => printOneOption(option, domObj));
+}
+
+//* --------------------------FILTER FUNCTIONS----------------------------------------->>
+
+//* --------------------------AUXILIARY FUNCTIONS----------------------------------------->>
+function capitalize(string) {
+    return string.at(0).toUpperCase() + string.slice(1, string.length);
+}
